681cd0719dd9f7bfc51b83e6480dbaaa976f815b
Taven Argus <daegontaven@gmail.com>
2019-11-20 12:53:42 +0530

    Add basic scaffold

---
+++ plugins/core/startup/login.py
@@ -0,0 +1,21 @@
+import discord
+
+from discord.ext import commands
+
+
+class Login(commands.Cog, name="Login"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.bot.logger.info(f"Logged in as: {self.bot.user.name}, {self.bot.user.id}")
+        await self.bot.change_presence(
+            activity=discord.Activity(
+                type=discord.ActivityType.watching, name="over Discord"
+            )
+        )
+
+
+def setup(bot):
+    bot.add_cog(Login(bot))
