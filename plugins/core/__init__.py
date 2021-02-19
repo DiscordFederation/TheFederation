60db2c733bffd7c3c157810f687b796d74ba64ac
taven <8206808+daegontaven@users.noreply.github.com>
2021-02-19 21:00:46 +0530

    Update to latest version of erin

--- plugins/core/__init__.py
+++ plugins/core/__init__.py
@@ -1,13 +1,14 @@
 from plugins.core.error import CommandError
 from plugins.core.navigation import HelpMenu
 from plugins.core.startup import Login
-from plugins.core.web import WebServer
 
-plugin_data = {"name": "Core Plugins"}
+plugin_data = {
+    "name": "Core Plugins",
+    "database": False
+}
 
 
 def setup(bot):
     bot.add_cog(CommandError(bot))
     bot.add_cog(HelpMenu(bot))
     bot.add_cog(Login(bot))
-    bot.add_cog(WebServer(bot))
--- plugins/core/web/__init__.py
+++
@@ -1,1 +0,0 @@
-from plugins.core.web.server import WebServer
